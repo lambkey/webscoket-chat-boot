b5eee72515c10d7138646042dd25b09021a6317b
lamb <2374785659@qq.com>
2025-08-01 16:54:11 +0800

    初始化项目

---
+++ src/main/java/com/lamb/websocket/config/WebResolveConfig.java
@@ -0,0 +1,24 @@
+package com.lamb.websocket.config;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author JoinYang
+ * @date 2024/4/15 16:12
+ * @Version 1.0
+ */
+@Component
+public class WebResolveConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedHeaders(CorsConfiguration.ALL)
+                .allowedMethods(CorsConfiguration.ALL)
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
---
+++ src/main/java/com/lamb/websocket/controller/TestController.java
@@ -0,0 +1,25 @@
+package com.lamb.websocket.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+/**
+ * @Author: yangzhuoying
+ * @CreateTime: 2025-08-01
+ * @Description: 测试
+ * @Version: 1.0
+ */
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+
+    @PostMapping
+    public Map result(){
+        return Map.of("result","success");
+    }
+
+}
